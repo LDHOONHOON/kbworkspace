05f6504087212aafd927c9da42705a1abfd4206a
이동훈 <zkwlao0@naver.com>
2024-06-18 18:33:33 +0900

    자바

---
+++ 04_JAVA/study/src/ch02/sec01/VariableInitalizationExample.java
@@ -0,0 +1,14 @@
+package ch02.sec01;
+
+public class VariableInitalizationExample {
+    public static void main(String[] args) {
+        //변수 value 선언
+        int value;
+
+        //연산 결과를 변수 result의 초기값으로 대입
+        int result = + 10; //에러
+
+        //변수 result 값을 읽고 콘솔에 출력
+        System.out.println(result);  //에러
+    }
+}
