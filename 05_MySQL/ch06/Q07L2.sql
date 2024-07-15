e1144f25b76df9adfff2e3063f577730afee4ee8
이동훈 <zkwlao0@naver.com>
2024-07-15 17:52:14 +0900

    sql08 테이블 Sql 10.1 JDBC프로그래밍

--- 05_MySQL/ch06/Q07L2.sql
+++ 05_MySQL/ch06/Q07L2.sql
@@ -33,3 +33,32 @@ ON s.stdName = sc.stdName
 INNER JOIN clubtbl c 
 ON c.clubName = sc.clubName
 ORDER BY s.stdName;
+
+-- 동아리 기준으로 가입한 학생의 목록을 출력하세요
+SELECT c.clubName, c.roomNo, s.stdName, s.addr
+FROM stdtbl s INNER JOIN stdclubtbl sc
+ON s.stdName = sc.stdName
+INNER JOIN clubtbl c 
+ON c.clubName = sc.clubName
+ORDER BY c.clubName;
+
+CREATE TABLE empTbl(emp CHAR(3), manager CHAR(3), empTel VARCHAR(8));
+
+INSERT INTO empTbl VALUES('나사장', NULL, '0000');
+INSERT INTO empTbl VALUES('김재무', '나사장', '2222');
+INSERT INTO empTbl VALUES('김부장', '김재무', '2222-1');
+INSERT INTO empTbl VALUES('이부장', '김재무', '2222-2');
+INSERT INTO empTbl VALUES('우대리', '이부장', '2222-2-1');
+INSERT INTO empTbl VALUES('지사원', '이부장', '2222-2-2');
+INSERT INTO empTbl VALUES('이영업', '나사장', '1111');
+INSERT INTO empTbl VALUES('한과장', '이영업', '1111-1');
+INSERT INTO empTbl VALUES('최정보', '나사장', '3333');
+INSERT INTO empTbl VALUES('윤차장', '최정보', '3333-1');
+INSERT INTO empTbl VALUES('이주임', '윤차장', '3333-1-1');
+
+-- 앞에서 추가한 테이블에서 우대리의 상관 연락처 정보를 확인하세요
+-- 동일한 테이블을 두개 만들어서 JOIN시킴 => SELF JOIN
+SELECT a.emp AS '부하직원', b.emp AS '직속상관', b.empTel AS '직속상관연락처'
+FROM emptbl a INNER JOIN emptbl b 
+ON a.manager = b.emp
+WHERE a.emp = '우대리';
